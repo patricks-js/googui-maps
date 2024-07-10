d8f6e93489631c83f1b277f1443f0515a5fc4466
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-07-10 13:03:14 -0300

    feat: setup cors plugin

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 
 const app = fastify({
   logger: true
+});
+
+app.register(fastifyCors, {
+  origin: "*"
 });
 
 app.get("/", async function handler(request, reply) {
@@ -10,6 +15,9 @@ app.get("/", async function handler(request, reply) {
 
 try {
   await app.listen({ port: 3333 });
+  const address = app.server.address();
+  const port = typeof address === "string" ? address : address?.port;
+  console.info(`Application listen on: ${port}`);
 } catch (err) {
   app.log.error(err);
   process.exit(1);
