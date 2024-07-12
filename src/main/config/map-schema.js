4bbb3cf728133a92be4a3ca7f1b3cab185ed4553
Lucas Glaydson <lucas.glaydson@jala.university>
2024-07-12 14:08:36 -0300

    feat: make openAPI map schema

---
+++ src/main/config/map-schema.js
@@ -0,0 +1,178 @@
+export const mapPostSchema = {
+  schema: {
+    tags: ["map"],
+    response: {
+      201: {
+        description: "Map created successfully",
+        type: "object",
+        properties: {
+          _id: { type: "string" },
+          name: { type: "string" },
+          dimensions: {
+            type: "object",
+            properties: {
+              width: { type: "number" },
+              height: { type: "number" }
+            }
+          },
+          obstacles: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                x: { type: "number" },
+                y: { type: "number" }
+              }
+            }
+          }
+        }
+      }
+    },
+    body: {
+      type: "object",
+      required: ["name", "dimensions", "obstacles"],
+      properties: {
+        name: { type: "string" },
+        dimensions: {
+          type: "object",
+          required: ["width", "height"],
+          properties: {
+            width: { type: "number" },
+            height: { type: "number" }
+          }
+        },
+        obstacles: {
+          type: "array",
+          items: {
+            type: "object",
+            required: ["x", "y"],
+            properties: {
+              x: { type: "number" },
+              y: { type: "number" }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+export const mapGetSchema = {
+  schema: {
+    tags: ["map"],
+    response: {
+      200: {
+        description: "Map retrieved successfully",
+        type: "object",
+        properties: {
+          _id: { type: "string" },
+          name: { type: "string" },
+          dimensions: {
+            type: "object",
+            properties: {
+              width: { type: "number" },
+              height: { type: "number" }
+            }
+          },
+          obstacles: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                x: { type: "number" },
+                y: { type: "number" }
+              }
+            }
+          }
+        }
+      }
+    },
+    params: {
+      type: "object",
+      required: ["id"],
+      properties: {
+        id: { type: "string" }
+      }
+    }
+  }
+};
+
+export const mapPutSchema = {
+  schema: {
+    tags: ["map"],
+    response: {
+      200: {
+        description: "Map updated successfully",
+        type: "object",
+        properties: {
+          _id: { type: "string" },
+          name: { type: "string" },
+          dimensions: {
+            type: "object",
+            properties: {
+              width: { type: "number" },
+              height: { type: "number" }
+            }
+          },
+          obstacles: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                x: { type: "number" },
+                y: { type: "number" }
+              }
+            }
+          }
+        }
+      }
+    },
+    params: {
+      type: "object",
+      required: ["id"],
+      properties: {
+        id: { type: "string" }
+      }
+    },
+    body: {
+      type: "object",
+      required: ["name", "dimensions", "obstacles"],
+      properties: {
+        name: { type: "string" },
+        dimensions: {
+          type: "object",
+          required: ["width", "height"],
+          properties: {
+            width: { type: "number" },
+            height: { type: "number" }
+          }
+        },
+        obstacles: {
+          type: "array",
+          items: {
+            type: "object",
+            required: ["x", "y"],
+            properties: {
+              x: { type: "number" },
+              y: { type: "number" }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+export const deleteMapSchema = {
+  schema: {
+    tags: ["map"],
+    response: { 204: {} },
+    params: {
+      type: "object",
+      required: ["id"],
+      properties: {
+        id: { type: "string" }
+      }
+    }
+  }
+};
--- src/main/routes/map.routes.js
+++ src/main/routes/map.routes.js
@@ -2,13 +2,19 @@ import { createMapControllers } from "../../http/controllers/map/create-map.js";
 import { deleteMapController } from "../../http/controllers/map/delete-map.js";
 import { findMapByIdController } from "../../http/controllers/map/find-map.js";
 import { updateMapController } from "../../http/controllers/map/update-map.js";
+import {
+  deleteMapSchema,
+  mapGetSchema,
+  mapPostSchema,
+  mapPutSchema
+} from "../config/map-schema.js";
 /**
  *
  * @param {import("fastify").FastifyInstance} app
  */
 export async function mapRoutes(app) {
-  app.post("/", createMapControllers);
-  app.get("/:id", findMapByIdController);
-  app.put("/:id", updateMapController); // Update map route can be added here if needed.
-  app.delete("/:id", deleteMapController);
+  app.post("/", mapPostSchema, createMapControllers);
+  app.get("/:id", mapGetSchema, findMapByIdController);
+  app.put("/:id", mapPutSchema, updateMapController); // Update map route can be added here if needed.
+  app.delete("/:id", deleteMapSchema, deleteMapController);
 }
