b6b09a840b4e2e872312fffe32d80aa8388c1228
Lucas Glaydson <lucas.glaydson@jala.university>
2024-07-16 09:48:48 -0300

    feat: create map and positions verify

--- src/main/routes/map.routes.js
+++ src/main/routes/map.routes.js
@@ -2,11 +2,13 @@ import { createMapControllers } from "../../http/controllers/map/create-map.js";
 import { deleteMapController } from "../../http/controllers/map/delete-map.js";
 import { findMapByIdController } from "../../http/controllers/map/find-map.js";
 import { updateMapController } from "../../http/controllers/map/update-map.js";
+import { verifyMapController } from "../../http/controllers/map/verify-map.js";
 import {
   deleteMapSchema,
   mapGetSchema,
   mapPostSchema,
-  mapPutSchema
+  mapPutSchema,
+  mapVerifySchema
 } from "../config/map-schema.js";
 /**
  *
@@ -17,4 +19,5 @@ export async function mapRoutes(app) {
   app.get("/:id", mapGetSchema, findMapByIdController);
   app.put("/:id", mapPutSchema, updateMapController); // Update map route can be added here if needed.
   app.delete("/:id", deleteMapSchema, deleteMapController);
+  app.post("/verify", mapVerifySchema, verifyMapController);
 }
