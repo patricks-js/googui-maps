89e604a47e0a72774f144111afd1a2fbbefa3edd
Lucas Glaydson <lucas.glaydson@jala.university>
2024-07-26 05:48:17 -0300

    test: creating usecase test of obstacles

--- src/data/usecases/obstacle/find-obstacle.js
+++ src/data/usecases/obstacle/find-obstacle.js
@@ -2,7 +2,7 @@ import { Obstacle } from "../../models/obstacle.js";
 
 export async function findObstacle(id) {
   try {
-    return Obstacle.findById(id);
+    return await Obstacle.findById(id);
   } catch (error) {
     throw new Error("Error fetching obstacles: ", error);
   }
