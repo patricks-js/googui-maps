ac843349af06c5a546c37eecf67517b4d2fd8770
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-07-28 11:24:04 -0300

    test: ensure that delete-path usecase implements new error handling

--- test/usecases/path/delete-path.test.js
+++ test/usecases/path/delete-path.test.js
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { Path } from "../../../src/data/models/path.js";
 import { deletePath } from "../../../src/data/usecases/path/delete-path.js";
+import { ServerError } from "../../../src/http/errors.js";
 
 vi.mock("../../../src/data/models/path.js");
 
@@ -17,31 +18,34 @@ describe("deletePath", () => {
   });
 
   it("should delete and return the path if it exists", async () => {
+    Path.findById = vi.fn().mockResolvedValue(mockPath._id);
     Path.findByIdAndDelete = vi.fn().mockResolvedValue(mockPath);
 
     const result = await deletePath(mockPath._id);
 
     expect(Path.findByIdAndDelete).toHaveBeenCalledWith(mockPath._id);
-    expect(result).toEqual(mockPath);
+    expect(result).toBeFalsy();
   });
 
   it("should throw an error if the path does not exist", async () => {
-    Path.findByIdAndDelete = vi.fn().mockResolvedValue(null);
+    Path.findById = vi.fn().mockResolvedValue(null);
 
     await expect(deletePath(mockPath._id)).rejects.toThrow(
-      "Error deleting path"
+      `Path with id ${mockPath._id} not found`
     );
 
-    expect(Path.findByIdAndDelete).toHaveBeenCalledWith(mockPath._id);
+    expect(Path.findById).toHaveBeenCalledWith(mockPath._id);
   });
 
   it("should throw an error if deleting the path fails", async () => {
-    const errorMessage = "Deleting error";
-    Path.findByIdAndDelete = vi.fn().mockRejectedValue(new Error(errorMessage));
+    const errorMessage = "Error deleting path";
 
-    await expect(deletePath(mockPath._id)).rejects.toThrow(
-      "Error deleting path"
-    );
+    Path.findById = vi.fn().mockResolvedValue(mockPath._id);
+    Path.findByIdAndDelete = vi
+      .fn()
+      .mockRejectedValue(new ServerError(errorMessage));
+
+    await expect(deletePath(mockPath._id)).rejects.toThrow(errorMessage);
 
     expect(Path.findByIdAndDelete).toHaveBeenCalledWith(mockPath._id);
   });
