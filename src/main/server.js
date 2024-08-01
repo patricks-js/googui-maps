4e07e55eac3eba8fa594aa1d4c7636963f3b1b84
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-08-01 11:25:54 -0300

    chore: remove comments from `server.js`

--- src/main/server.js
+++ src/main/server.js
@@ -2,16 +2,13 @@ import mongoose from "mongoose";
 import app from "./config/app.js";
 import { env } from "./config/env.js";
 
-// * Server
 const port = env.PORT || 3333;
 const host = "0.0.0.0";
 
 try {
-  app.log.info("Connecting to MongoDB...");
   await mongoose.connect(env.MONGODB_URL);
   app.log.info("Connected to MongoDB");
 
-  app.log.info("Starting server...");
   await app.listen({ port, host });
   app.log.info(`HTTP server up. Docs available at http://${host}:${port}/docs`);
 } catch (err) {
