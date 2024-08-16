eba4151eb367887334d366a88e7838a0a144b009
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-08-16 00:58:16 -0300

    chore(desktop): debloat desktop app

--- apps/desktop/src/renderer/src/App.tsx
+++ apps/desktop/src/renderer/src/App.tsx
@@ -1,6 +1,4 @@
 export function App() {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
     <>
       <div className="text-sm font-medium text-center">
@@ -9,15 +7,6 @@ export function App() {
       <div className="text-xl font-semibold text-center">
         Build an Electron app with <span>React</span>
         &nbsp;and TypeScript
-      </div>
-      <div className="flex justify-center">
-        <button
-          className="text-sm font-medium rounded px-3 py-1 border border-zinc-400 text-center"
-          type="button"
-          onClick={ipcHandle}
-        >
-          Send IPC
-        </button>
       </div>
     </>
   )
