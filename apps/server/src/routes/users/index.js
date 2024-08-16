5ea72d1ad4c936c45818c874ff44e6a5e7b54efa
Roger Rocha <roger.alves@jala.university>
2024-08-16 20:23:34 -0300

    feat(server): new user routes

--- apps/server/src/routes/users/index.js
+++ apps/server/src/routes/users/index.js
@@ -1,27 +1,44 @@
+import { z } from 'zod'
+import { createUser } from '../../../data/usecases/user/create-user.js'
+import { deleteUser } from '../../../data/usecases/user/delete-user.js'
+import { findUser } from '../../../data/usecases/user/find-user.js'
+import { updateUser } from '../../../data/usecases/user/update-user.js'
+import { validators } from '../../validators.js'
+
+const userSchema = z.object({
+  username: z.string(),
+  email: z.string().email(),
+  _id: z.string(),
+})
+
 /**
  *
  * @param {import("fastify").FastifyInstance} app
  */
-export default async function (app) {
-  const users = []
 
+export default async function (app) {
   app.post('/', async (request, reply) => {
-    reply.notFound('User not found')
+    const body = userSchema.parse(request.body)
+
+    return createUser(body)
   })
 
   app.get('/:id', async (request, reply) => {
-    return users.find((user) => user._id === request.params.id)
+    const { id } = validators.idParamSchema(request.params)
+
+    return findUser(id)
   })
 
   app.put('/:id', async (request, reply) => {
-    const userIndex = users.findIndex((user) => user._id === request.params.id)
-    users[userIndex] = request.body
-    return { message: 'User updated', users }
+    const { id } = validators.idParamSchema(request.params)
+    const changes = userSchema.parse(request.body)
+
+    return updateUser(id, changes)
   })
 
   app.delete('/:id', async (request, reply) => {
-    const userIndex = users.findIndex((user) => user._id === request.params.id)
-    users.splice(userIndex, 1)
-    return { message: 'User deleted', users }
+    const { id } = validators.idParamSchema(request.params)
+
+    return deleteUser(id)
   })
 }
